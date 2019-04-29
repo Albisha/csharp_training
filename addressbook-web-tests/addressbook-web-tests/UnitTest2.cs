d916e146615d5f1e5178f4a51822b906156cda5a
Albina <albina84@list.ru>
2019-04-30 01:08:59 +0300

    1ый работащий кейс (закомментировала несколько директив)

--- addressbook-web-tests/addressbook-web-tests/UnitTest2.cs
+++ addressbook-web-tests/addressbook-web-tests/UnitTest2.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
+/*using System.Text.RegularExpressions;
+using System.Threading;*/
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
+/* OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;*/
 
 namespace addressbook_web_tests
 {
