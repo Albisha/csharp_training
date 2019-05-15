6e0189c370fc3efeeeb47800ea9b9dfe01496b20
Albina <albina84@list.ru>
2019-05-16 00:29:15 +0300

    Создание базового класса для тестовых классов

---
+++ addressbook-web-tests/addressbook-web-tests/GroupRemovalTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+
+
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupRemovalTests: TestBase
+
+    {
+        
+        [Test]
+        public void GroupRemovalTest()
+        {
+            GoToHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToGroupsPage();
+            SelectGroup(1);
+            RemoveGroup();
+            ReturnToGroupsPage();
+        }
+
+    }
+}
