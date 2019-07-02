a59b4d022de6f297d90e2680bcc1b4716f9e8544
Albina <albina84@list.ru>
2019-07-03 01:37:06 +0300

    Задание №17: тесты для добавления контактов в группы и удаления контактов из групп с проверками

--- addressbook-web-tests/addressbook-web-tests/AddingContactToGroupTests.cs
+++ addressbook-web-tests/addressbook-web-tests/AddingContactToGroupTests.cs
@@ -12,69 +12,64 @@ namespace WebAddressbookTests
         [Test]
         public void AddingContactToGroupTest()
         {
-            List<GroupData> groups = GroupData.GetAll();
-            List<ContactData> allContactList = ContactData.GetAll();
             ContactData contact = new ContactData("1", "2");
             GroupData group=null;
+            int j = 0;
+          
+            app.Contact.IsContactExists();
+            app.Groups.IsGroupExists();
 
+            List<GroupData> groupsList = GroupData.GetAll();
+            List<ContactData> contacts = ContactData.GetAll();
 
+            int contactsCount = contacts.Count();
+            int groupsCount = groupsList.Count;
 
-            if (groups.Count == 0) //если нет групп, создаем новую
-            {
-                GroupData newgroup = new GroupData("grname");
-                newgroup.Header = "header";
-                newgroup.Footer = "footer";
-                app.Groups.Create(newgroup);
-                List<GroupData> newgroups = GroupData.GetAll();
-                groups = newgroups;
-                group = groups.First();
-            } 
- 
-            if (allContactList.Count == 0) //если нет контактов, создаем новый
-            {               
-                app.Contact.Create(contact);
-                allContactList.Add(contact);
-            }
 
             bool contactToAddExist = false;
                       
             List<ContactData> oldList = null;
-           // int count = groups.Count;
+            int i = 0; 
 
-            for (int i = 0; i < groups.Count; i++)
-            {
-                group = groups[i];
-                oldList = group.GetContacts();
+            while (i < groupsCount && !contactToAddExist) 
+            {               
+                oldList = groupsList[i].GetContacts(); 
                 if (oldList.Count == 0) //в группе нет контактов, добавляем первый из списка
                 {   
-                    contact = ContactData.GetAll().First();
-                    contactToAddExist = true;
+                    contact = ContactData.GetAll().First();                    
                 }
-                else if (oldList.Count == allContactList.Count) // в группу добавлены все контакты,п.э. создаем новый контакт
-                {
-                    
-                    contact = new ContactData("firstname123", "lastname123");
-                    app.Contact.Create(contact);
-                    contactToAddExist = true; 
+                else if (oldList.Count == contactsCount) // в группу добавлены все контакты,п.э. создаем новый контакт
+                {                    
+                    contact = new ContactData("firstname1234", "lastname123");                   
+                    app.Contact.Create(contact);                    
                 }
-                else if (oldList.Count < allContactList.Count) // есть контакт, который можно добавить в группу             
+                else if (oldList.Count < contactsCount) // есть контакт, который можно добавить в группу             
                 {                
-                  contact = ContactData.GetAll().Except(oldList).First();
-                    contactToAddExist = true;
-                }               
+                  contact = ContactData.GetAll().Except(oldList).First();                  
+                }
+                contacts[0] = contact;
+                group = groupsList[i];
+                contactToAddExist = true;
+                j = i;
+              
+                i++;
             }
 
             if (contactToAddExist)
-            {
-                app.Contact.AddContactToGrooup(contact, group);
-                List<ContactData> newList = group.GetContacts();
+            {   oldList = groupsList[j].GetContacts();
+                app.Contact.AddContactToGrooup(contacts[0], groupsList[j]);
+                List<ContactData> newList = groupsList[j].GetContacts();
                 oldList.Add(contact);
                 oldList.Sort();
                 newList.Sort();
                 Assert.AreEqual(oldList, newList);
 
             }
-                          
+            else
+            {
+                System.Console.Out.WriteLine("No contact to add to group");
+            }
+
         }
 
 
